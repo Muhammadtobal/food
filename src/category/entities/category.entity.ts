3331fc72ccbdfbabf668929d6d39c6523a2c7b78
alishadel2001 <alishadel2001@gmail.com>
2025-06-01 21:17:42 +0300

    fix relation category & item

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,20 +1,28 @@
-import { Item } from "src/item/entities/item.entity";
-import { Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Item } from 'src/item/entities/item.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 @Entity()
 export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-@PrimaryGeneratedColumn()
-id:number;
+  @Column({ nullable: false })
+  name: string;
 
-@Column({nullable:false})
-name:string;
+  @Column({ nullable: false })
+  image: string;
 
-@Column({nullable:false})
-image:string 
-
-@OneToMany(()=>Item,(item)=>item.category)
-items:Item
-@CreateDateColumn()
+  @OneToMany(() => Item, (item) => item.category)
+  items: Item;
+  @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
--- src/item/entities/item.entity.ts
+++ src/item/entities/item.entity.ts
@@ -1,21 +1,33 @@
-import { Category } from "src/category/entities/category.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Category } from 'src/category/entities/category.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 @Entity()
 export class Item {
-    @PrimaryGeneratedColumn()
-    id:number;
-    
-    @Column({nullable:false})
-    name:string;
-    
-    @Column()
-    image:string
-    
-    @ManyToOne(()=>Category,(category)=>category.items, { onDelete: 'SET NULL' })
-    category:Category;
-    @CreateDateColumn()
-      createdAt: Date;
-    
-      @UpdateDateColumn()
-      updatedAt: Date;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column()
+  image: string;
+
+  @ManyToOne(() => Category, (category) => category.items, {
+    onDelete: 'SET NULL',
+  })
+  category: Category;
+  @Column({ nullable: true })
+  categoryId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
